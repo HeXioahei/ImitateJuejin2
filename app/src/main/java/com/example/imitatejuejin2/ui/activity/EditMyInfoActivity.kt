118a27e06d3bd853ea4e71d5b4ba5631903875a8
HeXiaohei <3078158560@qq.com>
2024-03-01 00:25:31 +0800

    commit2

--- app/src/main/java/com/example/imitatejuejin2/ui/activity/EditMyInfoActivity.kt
+++ app/src/main/java/com/example/imitatejuejin2/ui/activity/EditMyInfoActivity.kt
@@ -1,12 +1,13 @@
 package com.example.imitatejuejin2.ui.activity
 
-import android.content.Context
-import android.graphics.BitmapFactory
-import android.net.Uri
+/**
+ *      desc     ： 编辑个人信息页面
+ *      author   ： hexiaohei
+ *      time     ： 2024/2/29
+ */
+
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Base64
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
@@ -19,26 +20,24 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.load.model.LazyHeaders
 import com.example.imitatejuejin2.R
+import com.example.imitatejuejin2.data.response.BaseResponse
 import com.example.imitatejuejin2.databinding.ActivityEditMyInfoBinding
 import com.example.imitatejuejin2.databinding.EditPasswordBinding
 import com.example.imitatejuejin2.model.AuthorBriefBuilder
 import com.example.imitatejuejin2.model.AuthorizationBuilder
+import com.example.imitatejuejin2.model.FileBuilder
 import com.example.imitatejuejin2.model.HasChanged
 import com.example.imitatejuejin2.model.ReLogin
 import com.example.imitatejuejin2.model.ServiceCreator
 import com.example.imitatejuejin2.requestinterface.mine.edit.EditHeadImageService
 import com.example.imitatejuejin2.requestinterface.mine.edit.EditPasswordService
 import com.example.imitatejuejin2.requestinterface.mine.edit.EditUsernameService
-import com.example.imitatejuejin2.data.response.BaseResponse
-import com.example.imitatejuejin2.model.FileBuilder
-import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import java.io.File
 
 class EditMyInfoActivity : AppCompatActivity() {
 
@@ -83,8 +82,7 @@ class EditMyInfoActivity : AppCompatActivity() {
                     Log.d("PhotoPicker", "Selected URI: $uri")
 
                     val headImageFile = FileBuilder.getImageFileFromUri(this, uri)
-                    //val requestBody = RequestBody.create(MediaType.parse("head_image"), headImageFile)
-                    val requestBody = RequestBody.create("head_image".toMediaTypeOrNull(), headImageFile)
+                    val requestBody = headImageFile.asRequestBody("head_image".toMediaTypeOrNull())
                     val multipartBody = MultipartBody.Part.createFormData("head_image", headImageFile.name, requestBody) // 这里的name（”head_image“）必须和接口文档里定义的参数名字一样
 
                     val appService = ServiceCreator.create(EditHeadImageService::class.java)
@@ -103,8 +101,9 @@ class EditMyInfoActivity : AppCompatActivity() {
                                         "更换成功",
                                         Toast.LENGTH_SHORT
                                     ).show()
-                                    Glide.with(this@EditMyInfoActivity).load(uri).into(binding.editHeadImageView)
-//                                    AuthorBriefBuilder.setHeadImage(Authorization)
+                                    Glide.with(this@EditMyInfoActivity)
+                                        .load(uri)
+                                        .into(binding.editHeadImageView)
                                     AuthorBriefBuilder.setAuthorBrief(Authorization)
                                     HasChanged.setHeadImageHasChangedValue(true)
                                 } else {
@@ -137,15 +136,15 @@ class EditMyInfoActivity : AppCompatActivity() {
             showInputUsername(username, Authorization)
         }
 
-
         // 更改密码
         binding.editPassword.setOnClickListener {
             showInputPassword(Authorization)
         }
     }
 
-
-
+    /**
+     * 更换用户名
+     */
     private fun showInputUsername(initText: String, Authorization: String) {
         val alertDialogBuilder = AlertDialog.Builder(this)
         alertDialogBuilder.setTitle("修改用户名")
@@ -157,7 +156,7 @@ class EditMyInfoActivity : AppCompatActivity() {
         alertDialogBuilder.setView(input)
 
         // 设置对话框的按钮
-        alertDialogBuilder.setPositiveButton("确定") { dialog, _ ->
+        alertDialogBuilder.setPositiveButton("确定") { _, _ ->
             val inputText = input.text.toString()
             // 在这里处理输入框中的文本
             val appService = ServiceCreator.create(EditUsernameService::class.java)
@@ -197,12 +196,15 @@ class EditMyInfoActivity : AppCompatActivity() {
         alertDialogBuilder.show()
     }
 
+    /**
+     * 更改密码
+     */
     private fun showInputPassword(Authorization: String) {
         val binding: EditPasswordBinding = EditPasswordBinding.inflate(layoutInflater)
         val alertDialogBuilder = AlertDialog.Builder(this)
         alertDialogBuilder.setView(binding.root)
 
-        alertDialogBuilder.setPositiveButton("确定") { dialog, _ ->
+        alertDialogBuilder.setPositiveButton("确定") { _, _ ->
             Log.d("old_password", binding.input1.text.toString())
             Log.d("new_password", binding.input2.text.toString())
             val appService = ServiceCreator.create(EditPasswordService::class.java)
@@ -241,24 +243,4 @@ class EditMyInfoActivity : AppCompatActivity() {
 
         alertDialogBuilder.show()
     }
-
-//    /**
-//     * 将图片由 uri 转换为 file
-//     */
-//    private fun getImageFileFromUri(context: Context, imageUri: Uri): File {
-//        val projection = arrayOf(MediaStore.Images.Media.DATA)
-//        val cursor = context.contentResolver.query(imageUri, projection, null, null, null)
-//
-//        if (cursor != null) {
-//            if (cursor.moveToFirst()) {
-//                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-//                val imagePath = cursor.getString(columnIndex)
-//                cursor.close()
-//                return File(imagePath)
-//            }
-//            cursor.close()
-//        }
-//
-//        return File("")
-//    }
 }
